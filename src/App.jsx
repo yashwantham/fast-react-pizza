c4cbdd43410eb22ad565f709b74abb13d009b6d9
Yashwanth <yashwanthyashu166@gmail.com>
2023-09-22 00:01:22 +0530

    built AppLayout and menu page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Home from "./ui/Home";
+import Error from "./ui/Error";
+import Menu, {loader as menuLoader} from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from './ui/AppLayout';
+
+// This way of defining routers is necessary in react router 6.4 in order to enable data fetching and data loading with react router 
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/", 
+        element: <Home />
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />
+      }
+    ]
+  }
+])
 
 function App() {
   return (
-    <div>Hello Vite!</div>
+    <RouterProvider router={router} />
   )
 }
 
